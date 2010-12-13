e98be2c51853a62088060cdfdc373387bcecba52
Diwant Vaidya <diwant@gmail.com>
2010-12-13 17:28:36 -0600

    Adding python final exam code

---
+++ cis345/final-01.py
@@ -0,0 +1,8 @@
+thing1 = "apple"
+thing2 = "orange"
+if thing1==thing2:
+	print thing1,"equals",thing2
+elif thing1>thing2:
+	print thing1,"is greater than",thing2
+else:
+	print thing1,"is less than",thing2
